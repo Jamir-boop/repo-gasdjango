88316b587d6e9281f42b80c3d724426afc970a06
Jamir Vargas <73477811+Jamir-boop@users.noreply.github.com>
2022-01-10 11:01:27 -0500

    static files  movido a BASE_DIR

--- VARGASERP/VARGASERP/settings.py
+++ VARGASERP/VARGASERP/settings.py
@@ -120,9 +120,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/image/'
-
+ROOT_PATH = os.path.dirname(__file__)
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static')
 ]
 
 # Default primary key field type
